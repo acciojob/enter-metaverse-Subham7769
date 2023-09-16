d45d60d2f231e2a1b511d629968346bcd827497b
Subham jain <134865513+Subham7769@users.noreply.github.com>
2023-09-16 23:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function Metaverse(){
+    document.getElementById("status").innerText = "Entered Metaverse";
+    document.getElementById("status").style.fontSize = "35px";
+    document.getElementById("status").style.fontWeight = "900";
+};

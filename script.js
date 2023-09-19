792761f28362899ff94ab640de9ecaae6e162134
Subham jain <134865513+Subham7769@users.noreply.github.com>
2023-09-19 12:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function Metaverse(){
-    document.getElementById("status").innerText = "Entered Metaverse";
-
-};
+let para = document.getElementById("status");
+let btn = document.getElementById("enterBtn");
+btn.addEventListener("click",() =>{
+para.innerHTML = "<h1>Entered Metaverse</h1>";
+});
